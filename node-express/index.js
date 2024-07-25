6601e0a794e6c79a87e55642058b6193458c6f13
190031138 <74003842+190031138@users.noreply.github.com>
2024-07-25 08:47:35 +0530

    Added Morgan and served static pages

--- node-express/index.js
+++ node-express/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const http = require('http');
 const port = 8080;
-
+const morgan = require('morgan');
 const hostname = 'localhost';
 
 const app = express();
-
+app.use(morgan('dev'));
+app.use(express.static(__dirname+'/public'));
 app.use((req,res,next) => {
-    console.log(req.headers);
+    //console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type','text/html');
     res.end('<html><body><h1>Welcome to my express server</h1></body></html>');
